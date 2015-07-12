2c8469c2353567a75e78fe093e3a69f9d5425c11
Jani Rahkola <jani.rahkola@iki.fi>
2015-07-12 20:15:20 +0300

    init

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject the-combiner "0.1.0-SNAPSHOT"
+  :description ""
+  :url "https://github.com/rahcola/the-combiner"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.7.0"]])
---
+++ src/the_combiner/core.clj
@@ -0,0 +1,1 @@
+(ns the-combiner.core)
